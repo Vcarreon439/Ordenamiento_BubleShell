ff43b0b3d8ff81c976eaf401d515010d0c54bb7c
Victor Hugo Carreon Pulido <hv_carreon@hotmail.com>
2020-12-01 21:27:20 -0600

    Funcional 1

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ namespace Ordenamiento_BubleShell
 
         static public void RBurbuja() 
         {
-            int[] ArrBurbu = { 15, 67, 08, 16, 44, 27, 12, 35 };
-            int[] copia = { 15, 67, 08, 16, 44, 27, 12, 35 };
+            int[] ArrBurbu = { 4, 8, 90, 23, 1, 67, 54, 16 };
+            int[] copia1 = { 4, 8, 90, 23, 1, 67, 54, 16 };
 
             Console.WriteLine("Metodo Burbuja\n");
             Console.WriteLine("Arreglo original");
@@ -29,13 +29,13 @@ namespace Ordenamiento_BubleShell
             Console.WriteLine("\nArreglo Ordenado");
             ArrBurbu.ImprimirEnL();
 
-            Utilidades.ProcedimientosBurbuja(copia);
+            Utilidades.ProcedimientosBurbuja(copia1);
         }
 
         static public void Shell()
         {
             int[] ArrShell = { 04, 08, 90, 23, 01, 67, 54, 16, 02, 85, 31, 114, 110, 26, 71, 38 };
-            int[] copia = { 04, 08, 90, 23, 01, 67, 54, 16, 02, 85, 31, 114, 110, 26, 71, 38 };
+            int[] copia2 = { 04, 08, 90, 23, 01, 67, 54, 16, 02, 85, 31, 114, 110, 26, 71, 38 };
 
             Console.WriteLine("Metodo Shell\n");
             Console.WriteLine("Arreglo original");
@@ -45,7 +45,7 @@ namespace Ordenamiento_BubleShell
             Console.WriteLine("\nArreglo Ordenado");
             ArrShell.ImprimirEnL();
 
-            Utilidades.ProcedimientosShell(copia);
+            Utilidades.ProcedimientosShell(copia2);
         }
 
     }
